e5b945ac1be3ad71e5f8b45f037fe0951afc5add
John Otander <johnotander@gmail.com>
2016-07-18 08:06:14 -0600

    Tweak active tab styling

--- index.js
+++ index.js
@@ -2,9 +2,22 @@
 
 const color = '#fafafa'
 const borderColor = '#111'
+const foregroundColor = '#fafafa'
 const backgroundColor = '#222'
 const cursorColor = 'rgba(200, 200, 200, .8)'
 
-const gray = { color, borderColor, backgroundColor, cursorColor }
+const gray = { color, borderColor, foregroundColor backgroundColor, cursorColor }
 
-module.exports.decorateConfig = config => Object.assign({}, config, gray)
+module.exports.decorateConfig = config => Object.assign({}, config, {
+  color, borderColor, foregroundColor backgroundColor, cursorColor,
+  css: `
+    ${config.css || ''}
+    .tab_tab.tab_active {
+      font-weight: 600;
+    }
+
+    .tab_tab.tab_active::before {
+      border-bottom-color: ${backgroundColor} !important;
+    }
+  `
+})
