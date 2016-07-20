4e3b17fa569a8a142d09a1581a174a915a7801ca
John Otander <johnotander@gmail.com>
2016-07-20 11:26:52 -0600

    Add more tab styling

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 'use strict'
 
 const color = '#fafafa'
-const borderColor = '#111'
+const borderColor = '#777'
 const foregroundColor = '#fafafa'
 const backgroundColor = '#222'
+const inactiveTabColor = '#111'
 const cursorColor = 'rgba(200, 200, 200, .8)'
 
 module.exports.decorateConfig = config => Object.assign({}, config, {
   color, borderColor, foregroundColor, backgroundColor, cursorColor,
   css: `
     ${config.css || ''}
+    .tab_tab {
+      color: ${borderColor} !important;
+    }
+
     .tab_tab.tab_active {
       font-weight: 600;
+      color: ${foregroundColor} !important;
+      background-color: ${backgroundColor} !important;
+    }
+
+    .tabs_nav {
+      background-color: ${inactiveTabColor} !important;
     }
 
     .tab_tab.tab_active::before {
